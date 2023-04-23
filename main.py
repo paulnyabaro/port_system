93aaf6354e301f771e2cbb0534edae5e12370fc2
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-23 20:21:13 +0300

    Added buttons

--- main.py
+++ main.py
@@ -2,12 +2,41 @@ import tkinter as tk
 
 app = tk.Tk()
 
-app.geometry('720x480')
+app.geometry('480x360')
 app.title('Port System Login')
 
+username_label = tk.Label(app, text='Username', font=('Arial', 14))
+username_label.pack(padx=10, pady=10)
+
+username_field = tk.Entry(app)
+username_field.pack(padx=20, pady=10)
+
+password_label = tk.Label(app, text='Password', font=('Arial', 14))
+password_label.pack(padx=10, pady=10)
+
+password_field = tk.Entry(app)
+password_field.pack(padx=20, pady=10)
+
+username_label = tk.Button(app, text='Login', font=('Arial', 14))
+username_label.pack(padx=20, pady=10)
+
+menuframe = tk.Frame(app)
+menuframe.columnconfigure(0, weight=1)
+menuframe.columnconfigure(1, weight=1)
+menuframe.columnconfigure(2, weight=1)
+
+menuitem1 = tk.Button(menuframe, text='Manage customers', font=('Arial', 14))
+menuitem1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+menuitem2 = tk.Button(menuframe, text='Search customer', font=('Arial', 14))
+menuitem2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+menuitem3 = tk.Button(menuframe, text='Logout', font=('Arial', 14))
+menuitem3.grid(row=0, column=2, sticky=tk.W+tk.E)
+
+menuframe.pack(fill='x')
 
 def login_user():
-    print('The username entered is ' + str(username_var) + 'and the password is ' + str(password_var))
     with open("login.txt", 'r') as file:
         next(file)
         
