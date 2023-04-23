31fb10dfb01db5bb76db4b7cb27a61e3c4e8d574
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-23 19:20:34 +0300

    Using default tkinter

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 import tkinter as tk
-import customtkinter
+
+app = tk.Tk()
+
+app.geometry('720x480')
+app.title('Port System Login')
+
 
 def login_user():
-    pass
+    print('The username entered is ' + str(username_var) + 'and the password is ' + str(password_var))
+    with open("login.txt", 'r') as file:
+        next(file)
+        
+        # iterate over the lines in the file
+        for line in file:
+            # remove any leading or trailing whitespace and split the line into fields
+            fields = line.strip().split(',')
+            
+            # extract the fields and assign them to variables
+            username = fields[0]
+            password = fields[1]
+            id = int(fields[2])
+            role = fields[3]
+            
+            # do something with the data, e.g. print it
+            print(f"Username: {username}, Password: {password}, ID: {id}, Role: {role}")
 
-# System settings
-customtkinter.set_appearance_mode("System") # Setting the dark or light mode from the system
-customtkinter.set_default_color_theme("blue")
 
 
-# Our app frame
-app = customtkinter.CTk() # Initializing the app
-app.geometry("720x480") # Setting the app size
-app.title("Port System Login")
-
-
-# Adding UI elements
-title = customtkinter.CTkLabel(app, text="Username")
-title.pack(padx=10, pady=10) # Padding for x and y
-
-# Link input
-username_var = tk.StringVar() # Getting the value of the link input
-username = customtkinter.CTkEntry(app, width=350, height=40, textvariable=username_var) # app is where to place it -> inside the app
-username.pack()
-
-# Adding UI elements
-title = customtkinter.CTkLabel(app, text="Password")
-title.pack(padx=10, pady=10) # Padding for x and y
-
-# Link input
-password_var = tk.StringVar() # Getting the value of the link input
-password = customtkinter.CTkEntry(app, width=350, height=40, textvariable=password_var) # app is where to place it -> inside the app
-password.pack()
-
-# Download button
-download = customtkinter.CTkButton(app, text="Login", command=login_user) # Run function
-download.pack(padx=20, pady=20) # .pack is used to make elements show up on the screen
 
 # Run app
 app.mainloop()
