59738e84a03ddb09b8e0c9ead423c94089f08a61
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-25 07:23:12 +0300

    Added logout menu

--- customs_officer.py
+++ customs_officer.py
@@ -29,6 +29,9 @@ new_passenger_button.pack(padx=5, pady=5)
 logout_button = tk.Button(option_menu_frame, text="Logout", command=logout_user)
 logout_button.pack(padx=5, pady=5)
 
+menubar = tk.Menu(root)
+menubar.add_cascade(label='Logout')
+root.config(menu=menubar)
 
 # run the main loop
 root.mainloop()
