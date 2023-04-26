9840f532cb74360cd5db9033fb3d440923537863
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-26 08:44:31 +0300

    Update imigration officer file

--- immigration_officer.py
+++ immigration_officer.py
@@ -13,6 +13,10 @@ def search_passenger():
     root.withdraw()
     os.system("python3 search_passenger.py")
 
+def existing_passenger():
+    root.withdraw()
+    os.system("python3 existing_passenger.py")
+
 def new_passenger():
     root.withdraw()
     os.system("python3 add_passenger.py")
@@ -24,13 +28,13 @@ def logout_user():
 option_menu_label = tk.Label(option_menu_frame, text="Select an option:")
 option_menu_label.pack(padx=5, pady=5)
 
-existing_passenger_button = tk.Button(option_menu_frame, text="Existing Passenger", command=search_passenger)
+existing_passenger_button = tk.Button(option_menu_frame, text="Existing Passenger", command=existing_passenger)
 existing_passenger_button.pack(padx=5, pady=5)
 
 new_passenger_button = tk.Button(option_menu_frame, text="New Passenger", command=new_passenger)
 new_passenger_button.pack(padx=5, pady=5)
 
-search_passenger_button = tk.Button(option_menu_frame, text="Search Passenger")
+search_passenger_button = tk.Button(option_menu_frame, text="Search Passenger", command=search_passenger)
 search_passenger_button.pack(padx=5, pady=5)
 
 menubar = tk.Menu(root)
