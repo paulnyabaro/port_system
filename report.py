ed12d9c01128d49ba1f656fd04295f91bd006786
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-27 09:31:16 +0300

    Updated report

--- report.py
+++ report.py
@@ -24,20 +24,39 @@ passengers_with_fine_over_label.grid(row=3, column=0)
 with open("passenger.txt", "r") as f:
     passengers = json.load(f)
 
+    def passengers_with_fine(passengers,amount=1):
+        global count
+        count = 0
+        for passenger in passengers.values():
+            fine = int(passenger[3])
+            if fine > amount:
+                count += 1
+        return count
+    
+    def calculate_total_fine(passengers):
+        total_fine = 0
+        for passenger in passengers.values():
+            fine = passenger[3]
+            if fine.isdigit() and int(fine) > 0:
+                total_fine += int(fine)
+        return total_fine
+    
+
+
 # create a function to display the passenger information
 # def display_info():
     # get the civil id entered by the user
     # civil_id = entry.get()
 
     # info = passengers[civil_id]
-total_passengers_fined = 10
-average_fine_per_passenger = 1500
-passengers_with_fine_over = 5
+total_passengers_fined = passengers_with_fine(passengers)
+average_fine_per_passenger = calculate_total_fine(passengers)
+passengers_with_fine_over = passengers_with_fine(passengers,6000)
 # customs_fine = info[3]
 
 # display the passenger information on the screen
 total_passengers_fined_label.config(text="Total passengers fined: " + str(total_passengers_fined))
-average_fine_per_passenger_label.config(text="Average fine per passenger: " + str(average_fine_per_passenger) + " KD")
+average_fine_per_passenger_label.config(text="Average fine per passenger: " + str(average_fine_per_passenger / total_passengers_fined) + " KD")
 passengers_with_fine_over_label.config(text="Passengers with fine more than 6000 KD: " + str(passengers_with_fine_over))
 
 def set_status(status):
